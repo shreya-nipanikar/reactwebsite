0db364e32398e95d390eb2d9d9ae93377aa32140
shreya-nipanikar <nipanikar.shreya1@gmail.com>
2020-12-28 17:50:30 +0530

    Updates

---
+++ precache-manifest.0bf35c0ad47426df06da293a96707ab3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023e346c93bcd953385a86f8ae88019f",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "c26a7216dae4780abb68",
+    "url": "/reactwebsite/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "768557124df5a4e3a408",
+    "url": "/reactwebsite/static/css/main.5b837197.chunk.css"
+  },
+  {
+    "revision": "c26a7216dae4780abb68",
+    "url": "/reactwebsite/static/js/2.0d43423e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactwebsite/static/js/2.0d43423e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768557124df5a4e3a408",
+    "url": "/reactwebsite/static/js/main.4acc3cf9.chunk.js"
+  },
+  {
+    "revision": "6aab119af7a640187c80",
+    "url": "/reactwebsite/static/js/runtime-main.307e3004.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactwebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
